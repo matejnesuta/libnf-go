71c1ab31313abf064a4bd93318660fd9789f35f5
matejnesuta <matejnetusa@gmail.com>
2025-03-09 01:41:41 +0100

    ring support and initial tests for memheap

--- api/memheap/memheap_test.go
+++ api/memheap/memheap_test.go
@@ -4,6 +4,10 @@ import (
 	"libnf/api/memheap"
 	"testing"
 
+	LnfErr "libnf/api/errors"
+	LnfFld "libnf/api/fields"
+	LnfRec "libnf/api/record"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,13 +16,132 @@ func TestNewMemHeap(t *testing.T) {
 	assert.Equal(t, nil, err)
 	assert.Equal(t, true, memHeap.Allocated())
 
-	// if err != nil {
-	// 	t.Errorf("NewMemHeap() failed: %v", err)
-	// }
 	err = memHeap.Free()
 	assert.Equal(t, nil, err)
 	assert.Equal(t, false, memHeap.Allocated())
-	// if err != nil {
-	// 	t.Errorf("MemHeap.Free() failed: %v", err)
-	// }
+}
+
+func TestMemHeapDoubleFree(t *testing.T) {
+	memHeap, err := memheap.NewMemHeap()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, memHeap.Allocated())
+
+	err = memHeap.Free()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, false, memHeap.Allocated())
+
+	err = memHeap.Free()
+	assert.Equal(t, LnfErr.ErrMemHeapNotAllocated, err)
+	assert.Equal(t, false, memHeap.Allocated())
+}
+
+func TestReadFromEmptyMemHeap(t *testing.T) {
+	memHeap, err := memheap.NewMemHeap()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, memHeap.Allocated())
+
+	rec, _ := LnfRec.NewRecord()
+	err = memHeap.GetNextRecord(&rec)
+	assert.Equal(t, LnfErr.ErrMemHeapEnd, err)
+}
+
+func TestReadFromUsingUnallocatedRecord(t *testing.T) {
+	memHeap, err := memheap.NewMemHeap()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, memHeap.Allocated())
+
+	rec := LnfRec.Record{}
+	err = memHeap.GetNextRecord(&rec)
+	assert.Equal(t, LnfErr.ErrRecordNotAllocated, err)
+}
+
+func TestReadFromFreedMemHeap(t *testing.T) {
+	memHeap, err := memheap.NewMemHeap()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, memHeap.Allocated())
+
+	err = memHeap.Free()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, false, memHeap.Allocated())
+
+	rec, _ := LnfRec.NewRecord()
+	err = memHeap.GetNextRecord(&rec)
+	assert.Equal(t, LnfErr.ErrMemHeapNotAllocated, err)
+}
+
+func TestSetAggrOptionsToFreedMemHeap(t *testing.T) {
+	memHeap, err := memheap.NewMemHeap()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, memHeap.Allocated())
+
+	err = memHeap.Free()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, false, memHeap.Allocated())
+
+	err = memHeap.SetAggrOptions(LnfFld.FldSrcport, memheap.AggrKey, memheap.SortAsc, 0, 0)
+	assert.Equal(t, LnfErr.ErrMemHeapNotAllocated, err)
+}
+
+func TestWriteToFreedMemHeap(t *testing.T) {
+	memHeap, err := memheap.NewMemHeap()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, memHeap.Allocated())
+
+	err = memHeap.Free()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, false, memHeap.Allocated())
+
+	rec, _ := LnfRec.NewRecord()
+	err = memHeap.WriteRecord(&rec)
+	assert.Equal(t, LnfErr.ErrMemHeapNotAllocated, err)
+}
+
+func TestWriteToMemHeap(t *testing.T) {
+	memHeap, _ := memheap.NewMemHeap()
+	memHeap.SetAggrOptions(LnfFld.FldSrcport, memheap.AggrKey, memheap.SortAsc, 0, 0)
+
+	rec, _ := LnfRec.NewRecord()
+	LnfRec.SetField(&rec, LnfFld.FldSrcport, uint16(80))
+
+	err := memHeap.WriteRecord(&rec)
+	assert.Equal(t, nil, err)
+
+	rec2, _ := LnfRec.NewRecord()
+	err = memHeap.GetNextRecord(&rec2)
+	assert.Equal(t, nil, err)
+
+	val, _ := rec2.GetField(LnfFld.FldSrcport)
+	srcport := val.(uint16)
+	assert.Equal(t, uint16(80), srcport)
+}
+
+func TestCleanFreedMemHeap(t *testing.T) {
+	memHeap, err := memheap.NewMemHeap()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, memHeap.Allocated())
+
+	err = memHeap.Free()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, false, memHeap.Allocated())
+
+	err = memHeap.Clear()
+	assert.Equal(t, LnfErr.ErrMemHeapNotAllocated, err)
+}
+
+func TestCleanMemHeap(t *testing.T) {
+	memHeap, _ := memheap.NewMemHeap()
+	memHeap.SetAggrOptions(LnfFld.FldSrcport, memheap.AggrKey, memheap.SortAsc, 0, 0)
+
+	rec, _ := LnfRec.NewRecord()
+	LnfRec.SetField(&rec, LnfFld.FldSrcport, uint16(80))
+
+	err := memHeap.WriteRecord(&rec)
+	assert.Equal(t, nil, err)
+
+	err = memHeap.Clear()
+	assert.Equal(t, nil, err)
+
+	rec2, _ := LnfRec.NewRecord()
+	err = memHeap.GetNextRecord(&rec2)
+	assert.Equal(t, LnfErr.ErrMemHeapEnd, err)
 }
---
+++ api/ring/ring.go
@@ -0,0 +1,82 @@
+package ring
+
+import (
+	LnfErr "libnf/api/errors"
+	LnfRec "libnf/api/record"
+	"libnf/internal"
+	"unsafe"
+)
+
+type Ring struct {
+	ptr uintptr
+}
+
+const (
+	RingTotal int = internal.RING_TOTAL
+	RingLost  int = internal.RING_LOST
+	RingStuck int = internal.RING_STUCK
+)
+
+func NewRing(filename string, forceInit bool, forceRelease bool, nonBlockingReading bool) (Ring, error) {
+	ring := Ring{}
+	var flags int
+	if forceInit {
+		flags |= internal.RING_FORCE_INIT
+	}
+	if forceRelease {
+		flags |= internal.RING_FORCE_RELEASE
+	}
+	if nonBlockingReading {
+		flags |= internal.RING_NO_BLOCK
+	}
+
+	status := internal.Ring_init(&ring.ptr, filename, flags)
+	if status == internal.ERR_NOMEM {
+		return ring, LnfErr.ErrNoMem
+	} else if status == internal.ERR_OTHER {
+		return ring, LnfErr.ErrOther
+	}
+	return ring, nil
+}
+
+func (r *Ring) Info(infoType int) (int, error) {
+	var info uint64
+	status := internal.Ring_info(r.ptr, infoType, uintptr(unsafe.Pointer(&info)), int64(8))
+	if status == internal.ERR_OTHER {
+		return 0, LnfErr.ErrOther
+	}
+	return status, nil
+}
+
+func (r *Ring) GetNextRecord(rec *LnfRec.Record) error {
+	if !r.Allocated() {
+		return LnfErr.ErrRecordNotAllocated
+	}
+
+	status := internal.Ring_read(r.ptr, rec.GetPtr())
+	if status == internal.EOF {
+		return LnfErr.ErrFileEof
+	} else if status == internal.ERR_OTHER {
+		return LnfErr.ErrOther
+	}
+	return nil
+}
+
+func (r *Ring) WriteRecord(rec *LnfRec.Record) error {
+	if !rec.Allocated() {
+		return LnfErr.ErrRecordNotAllocated
+	}
+
+	status := internal.Ring_write(r.ptr, rec.GetPtr())
+	if status == internal.ERR_NOMEM {
+		return LnfErr.ErrNoMem
+	} else if status == internal.ERR_OTHER {
+		return LnfErr.ErrOther
+	}
+	return nil
+}
+
+func (r *Ring) Free() error {
+	internal.Ring_free(r.ptr)
+	return nil
+}
